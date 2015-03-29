90ec61efdce2053b5d7636365a15746c4c40461b
Vladyslav Platonov <asdixer@gmail.com>
2015-03-29 15:20:16 +0300

    dev

--- README.md
+++ README.md
@@ -28,4 +28,4 @@ Usage
 Once the extension is installed, simply use it in your code by  :
 
 ```php
-<?= \platx\contentblock\AutoloadExample::widget(); ?>```
+<?= \platx\contentblock\Widget::widget(); ?>```
--- Widget.php
+++ Widget.php
@@ -1,16 +1,39 @@
 <?php
 
-namespace platx\contentBlockWidget;
+namespace platx\contentblock;
+use yii\db\ActiveQuery;
+use yii\helpers\Html;
 
 /**
  * This is just an example.
  */
 class Widget extends \yii\base\Widget
 {
-    public $
+    public $listTag = 'ul';
+
+    public $listOptions = ['class' => 'content-block'];
+
+    public $itemTag = 'li';
+
+    public $itemOptions = ['class' => 'item'];
+
+    public $query = null;
+
 
     public function run()
     {
-        return "Hello!";
+        /** @var ActiveQuery $query */
+        $query = $this->query;
+        $models = $query->all();
+
+        $items = [];
+
+        foreach($models as $model) {
+            $items[] = Html::tag($this->itemTag, $model->name, $this->itemOptions);
+        }
+
+        $content = implode('', $items);
+
+        return Html::tag($this->listTag, $content, $this->listOptions);
     }
 }
