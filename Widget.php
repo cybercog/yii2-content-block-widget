1d8bd5902a56847a906f1ecf5aaddf63112bcfda
Vladyslav Platonov <asdixer@gmail.com>
2015-03-29 14:36:45 +0300

    dev

--- Widget.php
+++ Widget.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace platx\contentblock;
+namespace platx\contentBlockWidget;
 
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class Widget extends \yii\base\Widget
 {
+    public $
+
     public function run()
     {
         return "Hello!";
